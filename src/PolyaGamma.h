c23bbb82c29d609632c4e7e6f1ed84383058d77a
jesseHFG <jesse@hifidelitygenetics.com>
2019-09-24 10:12:46 -0400

    Updated header in .h files

--- src/PolyaGamma.h
+++ src/PolyaGamma.h
@@ -1,23 +1,20 @@
-// -*- mode: c++; -*-
-
-////////////////////////////////////////////////////////////////////////////////
-
-// Copyright 2012 Nick Polson, James Scott, and Jesse Windle.
+// -*- mode: c++;  c-basic-offset: 4 -*-
+// (C) Nicholas Polson, James Scott, Jesse Windle, 2012-2019
 
 // This file is part of BayesLogit.
 
 // BayesLogit is free software: you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
-// Foundation, either version 3 of the License, or any later version.
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
 
 // BayesLogit is distributed in the hope that it will be useful, but WITHOUT ANY
 // WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
 // A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License along with
-// BayesLogit.  If not, see <http://www.gnu.org/licenses/>.
+// BayesLogit.  If not, see <https://www.gnu.org/licenses/>.
 
-////////////////////////////////////////////////////////////////////////////////
 
 // See <http://arxiv.org/abs/1205.0310> for implementation details.
 
