27b34267e18a1c974f54617c9d6f48d576c230f8
jesseHFG <jesse@hifidelitygenetics.com>
2019-09-24 23:32:40 -0400

    Fixed bug in simple_RNG_wrapper (did not paranthesis all macro variables)

--- src/simple_RNG_wrapper.h
+++ src/simple_RNG_wrapper.h
@@ -40,16 +40,16 @@ inline void check_R_interupt(int count)
 
 // CDF
 #define p_norm(X, USE_LOG) pnorm((X), 0.0, 1.0, true, (USE_LOG))
-#define p_gamma_scale(X, SHAPE, SCALE, USE_LOG) pgamma((X), (SHAPE), (SCALE), 1., (USE_LOG))
-#define p_gamma_rate(X, SHAPE, RATE, USE_LOG) pgamma((X), (SHAPE), (1./RATE), 1., (USE_LOG))
+#define p_gamma_scale(X, SHAPE, SCALE, USE_LOG) pgamma((X), (SHAPE), (SCALE), 1, (USE_LOG))
+#define p_gamma_rate(X, SHAPE, RATE, USE_LOG) pgamma((X), (SHAPE), (1./(RATE)), 1, (USE_LOG))
 
 // Random variates
 #define expon_mean(MEAN) rexp((MEAN))
-#define expon_rate(RATE) rexp((1./RATE))
+#define expon_rate(RATE) rexp((1./(RATE)))
 #define unif() runif(0.0, 1.0)
 #define norm(MEAN, SD) rnorm((MEAN), (SD))
 #define gamma_scale(SHAPE, SCALE) rgamma((SHAPE), (SCALE))
-#define gamma_rate(SHAPE, RATE) rgamma((SHAPE), (1./RATE))
+#define gamma_rate(SHAPE, RATE) rgamma((SHAPE), (1./(RATE)))
 
 // Scientific functions
 #define lgamma(X) lgammafn((X))
